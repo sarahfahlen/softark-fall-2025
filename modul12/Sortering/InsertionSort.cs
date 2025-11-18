97391ea6cedbf9f5229503eb72fdc8961469b96b
Sarah Fahlén <sarahfahlen@MacBook-Air.local>
2025-11-18 08:24:25 +0100

    modul 12

--- modul12/Sortering/InsertionSort.cs
+++ modul12/Sortering/InsertionSort.cs
@@ -5,7 +5,26 @@ public class InsertionSort
 
     public static void Sort(int[] array)
     {
-        // TODO!
+        for (int i = 1; i < array.Length; i++)
+        {
+            int next = array[i];
+            
+            int j = i;
+            bool found = false;
+            while (!found && j > 0)
+            {
+                if (next >= array[j - 1])
+                {
+                    found = true;
+                }
+                else
+                {
+                    array[j] = array[j - 1];
+                    j--;
+                }
+            }
+            array[j] = next;
+        }
         return;
     }
 }
--- modul12/Sortering/SelectionSort.cs
+++ modul12/Sortering/SelectionSort.cs
@@ -11,7 +11,17 @@ public class SelectionSort
 
     public static void Sort(int[] array)
     {
-        // TODO!
-        return;
+        for (int i = 0; i < array.Length; i++)
+        {
+            int min = i;
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[j] < array[min])
+                {
+                    min = j;
+                }
+            }
+            Swap(array, i, min);
+        }
     }
 }
